28317083485f01014205f5d375c543abd98e9b24
Ryuichi Hieda <ryu19912@gmail.com>
2019-04-26 15:58:47 +0900

    Initial Commit.

---
+++ SwiftFormatSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFormatSample
+//
+//  Created by hiedar on 2019/04/26.
+//  Copyright © 2019年 Festil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
